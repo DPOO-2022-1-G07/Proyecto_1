98ad5bec1fc16cd84028ab1ecf0fdfec26210985
Juan Andrés Eslava Tovar <88861582+juaneslava@users.noreply.github.com>
2022-04-08 17:13:51 -0500

    modificación juan

--- Entrega 3/src/uniandes/dpoo/proyecto1/modelo/Reporte.java
+++ Entrega 3/src/uniandes/dpoo/proyecto1/modelo/Reporte.java
@@ -1,5 +1,28 @@
 package uniandes.dpoo.proyecto1.modelo;
 
-public class Reporte {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
 
+
+public class Reporte {
+	
+	/*private List<Actividad> actividad;
+	private Proyecto proyecto;*/
+	
+	
+	private void ejecutarConsultarPedido() throws IOException 
+	{
+		BufferedReader br = new BufferedReader(new FileReader("./data/proyecto.txt"));
+		String linea = br.readLine();
+		while (linea != null) // Cuando se llegue al final del archivo, linea tendrá el valor null
+		{
+			linea = br.readLine();
+			String[] parts = linea.split(";");
+			String part1 = parts[7];
+		}
+		br.close();
+	}
+	
 }
